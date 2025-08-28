9045bad0c49861c5fa0e88799e7b2733534f3583
ateetvatan <ateetph@gmail.com>
2025-08-29 00:47:33 +0200

    inits

--- app/etl/tasks/__init__.py
+++ app/etl/tasks/__init__.py
@@ -6,10 +6,12 @@ from .news_content_extractor import NewsContentExtractor
 from .summarizer import Summarizer
 from .vectorize_articles import VectorizeArticles
 from .cluster_summary_generator import ClusterSummaryGenerator
+from .summarizer_utils import SummarizerUtils
 
 __all__ = [
     "NewsContentExtractor",
     "Summarizer",
     "VectorizeArticles",
     "ClusterSummaryGenerator",
+    "SummarizerUtils",
 ]
